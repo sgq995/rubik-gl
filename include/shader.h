496e55606c3bfe2c13999128919629d71a9eb0d8
Sebastián <sgq995@gmail.com>
2020-01-03 14:26:42 -0500

    feat: Added shader to handling linking

---
+++ include/shader.h
@@ -0,0 +1,24 @@
+#ifndef SHADER_H_
+#define SHADER_H_
+
+#include <GL/glew.h>
+
+#include "shader_object_pool.h"
+
+class Shader {
+public:
+  Shader();
+  ~Shader();
+
+  bool Attach(const ShaderObject& source);
+  bool Link();
+
+  void Bind();
+
+  const GLuint& program() const;
+
+private:
+  GLuint program_ = 0;
+};
+
+#endif  // SHADER_H_
---
+++ src/shader.cc
@@ -0,0 +1,51 @@
+#include "shader.h"
+
+#include <iostream>
+
+Shader::Shader() {
+  program_ = glCreateProgram();
+}
+
+Shader::~Shader() {
+
+}
+
+
+bool Shader::Attach(const ShaderObject& object) {
+  glAttachShader(program_, object.id);
+
+  return true;
+}
+
+bool Shader::Link() {
+  glLinkProgram(program_);
+
+  GLint link_status;
+  glGetProgramiv(program_, GL_LINK_STATUS, &link_status);
+  if (link_status == GL_FALSE) {
+    GLint size;
+    glGetProgramiv(program_, GL_INFO_LOG_LENGTH, &size);
+
+    GLchar *log = new GLchar[size];
+    glGetProgramInfoLog(program_, size, NULL, log);
+    std::cerr << "GL program failed: " << log << std::endl;
+    delete[] log;
+
+    glDeleteProgram(program_);
+    program_ = glCreateProgram();
+
+    return false;
+  }
+
+  return true;
+}
+
+
+void Shader::Bind() {
+  glUseProgram(program_);
+}
+
+
+const GLuint& Shader::program() const {
+  return program_;
+}
