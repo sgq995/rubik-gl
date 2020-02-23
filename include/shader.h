ab5f11b88319b68465f2466742f64b98740563e3
Sebastián <sgq995@gmail.com>
2020-02-22 19:56:18 -0500

    feat: Added map to shader to store locations for attributes and uniforms

--- include/shader.h
+++ include/shader.h
@@ -1,6 +1,8 @@
 #ifndef SHADER_H_
 #define SHADER_H_
 
+#include <unordered_map>
+
 #include <GL/glew.h>
 
 #include "shader_object_pool.h"
@@ -13,12 +15,22 @@ public:
   bool Attach(const ShaderObject &source);
   bool Link();
 
-  void Use();
+  void Use() const;
+
+  GLint AttribLocation(const GLchar *name);
+  GLint UniformLocation(const GLchar *name);
 
   const GLuint& program() const;
 
 private:
+  void DetachShaders() const;
+
+  bool linked_ = false;
+
   GLuint program_ = 0;
+
+  std::unordered_map<const GLchar *, GLint> attribs_;
+  std::unordered_map<const GLchar *, GLint> uniforms_;
 };
 
 #endif  // SHADER_H_
--- src/shader.cc
+++ src/shader.cc
@@ -7,6 +7,7 @@ Shader::Shader() {
 }
 
 Shader::~Shader() {
+  DetachShaders();
   glDeleteProgram(program_);
 }
 
@@ -18,6 +19,10 @@ bool Shader::Attach(const ShaderObject &object) {
 }
 
 bool Shader::Link() {
+  if (linked_) {
+    return true;
+  }
+
   glLinkProgram(program_);
 
   GLint link_status;
@@ -31,21 +36,54 @@ bool Shader::Link() {
     std::cerr << "GL program failed: " << log << std::endl;
     delete[] log;
 
+    DetachShaders();
     glDeleteProgram(program_);
     program_ = glCreateProgram();
 
+    linked_ = false;
     return false;
   }
 
+  linked_ = true;
   return true;
 }
 
 
-void Shader::Use() {
+void Shader::Use() const {
   glUseProgram(program_);
+}
+
+
+GLint Shader::AttribLocation(const GLchar *name) {
+  if (attribs_.find(name) == attribs_.end()) {
+    attribs_[name] = glGetAttribLocation(program_, name);
+  }
+
+  return attribs_[name];
+}
+
+GLint Shader::UniformLocation(const GLchar *name) {
+  if (uniforms_.find(name) == uniforms_.end()) {
+    uniforms_[name] = glGetUniformLocation(program_, name);
+  }
+
+  return uniforms_[name];
 }
 
 
 const GLuint& Shader::program() const {
   return program_;
+}
+
+
+void Shader::DetachShaders() const {
+  GLint attached_shaders = 0;
+  glGetProgramiv(program_, GL_ATTACHED_SHADERS, &attached_shaders);
+
+  GLuint *shaders = new GLuint[attached_shaders];
+  glGetAttachedShaders(program_, attached_shaders, NULL, shaders);
+
+  for (GLint i = 0; i < attached_shaders; ++i) {
+    glDetachShader(program_, shaders[i]);
+  }
 }
