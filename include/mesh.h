21a89bc5ef29c9aa886584efa000fecda5783cef
Sebastián <sgq995@gmail.com>
2020-03-08 20:04:42 -0500

    feat: Added mesh class as verticies container

---
+++ include/mesh.h
@@ -0,0 +1,45 @@
+#ifndef MESH_H_
+#define MESH_H_
+
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include "buffer.h"
+#include "buffer_layout.h"
+#include "renderer.h"
+#include "shader.h"
+
+class Mesh {
+public:
+  Mesh();
+  virtual ~Mesh();
+
+  void Scale(const glm::vec3 &scaling);
+  void Translate(const glm::vec3 &translation);
+  void Rotate(const glm::f32 &angle, const glm::vec3 &rotation);
+
+  virtual void Draw(const Renderer &renderer);
+
+  const Buffer& vertex_buffer() const;
+  
+  const Buffer& index_buffer() const;
+
+  const BufferLayout& layout() const;
+
+  const glm::mat4& model_view() const;
+  void set_model_view(const glm::mat4 &model_view);
+  const GLfloat *model_view_ptr() const;
+
+protected:
+  void CreateVertexBuffer(GLuint index, const std::vector<GLfloat> &verticies);
+  void CreateIndexBuffer(const std::vector<GLuint> &indicies);
+
+private:
+  Buffer vertex_buffer_;
+  Buffer index_buffer_;
+  BufferLayout layout_;
+  glm::mat4 model_view_;
+};
+
+#endif  // MESH_H_
---
+++ src/mesh.cc
@@ -0,0 +1,94 @@
+#include "mesh.h"
+
+#include <iostream>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+Mesh::Mesh() 
+    : model_view_(1.0f) {
+
+}
+
+Mesh::~Mesh() {
+
+}
+
+
+void Mesh::Scale(const glm::vec3 &scaling) {
+  model_view_ = glm::scale(model_view_, scaling);
+}
+
+void Mesh::Translate(const glm::vec3 &translation) {
+  model_view_ = glm::translate(model_view_, translation);
+}
+
+void Mesh::Rotate(const glm::f32 &angle, const glm::vec3 &rotation) {
+  model_view_ = glm::rotate(model_view_, angle, rotation);
+}
+
+
+void Mesh::Draw(const Renderer &renderer) {
+  layout_.Bind();
+  renderer.DrawElements(GL_TRIANGLES, index_buffer_);
+}
+
+
+const Buffer& Mesh::vertex_buffer() const {
+  return vertex_buffer_;
+}
+  
+
+const Buffer& Mesh::index_buffer() const {
+  return index_buffer_;
+}
+
+
+const BufferLayout& Mesh::layout() const {
+  return layout_;
+}
+
+
+const glm::mat4& Mesh::model_view() const {
+  return model_view_;
+}
+
+void Mesh::set_model_view(const glm::mat4 &model_view) {
+  model_view_ = model_view;
+}
+
+const GLfloat *Mesh::model_view_ptr() const {
+  return glm::value_ptr(model_view_);
+}
+
+
+void Mesh::CreateVertexBuffer(GLuint index, const std::vector<GLfloat> &verticies) {
+  Buffer::Properties props = {
+    .target = GL_ARRAY_BUFFER,
+    .type = GL_FLOAT,
+    .size = 3,
+  };
+
+  Buffer::Pointer ptr = {
+    .data = verticies.data(),
+    .size = static_cast<GLsizeiptr>(verticies.size() * sizeof(GLfloat)),
+  };
+  
+  vertex_buffer_.Create(props, ptr);
+  layout_.Attrib(index, vertex_buffer_);
+}
+
+void Mesh::CreateIndexBuffer(const std::vector<GLuint> &indicies) {
+  Buffer::Properties props = {
+    .target = GL_ELEMENT_ARRAY_BUFFER,
+    .type = GL_UNSIGNED_INT,
+    .count = static_cast<GLsizei>(indicies.size()),
+  };
+
+  Buffer::Pointer ptr = {
+    .data = indicies.data(),
+    .size = static_cast<GLsizeiptr>(indicies.size() * sizeof(GLfloat)),
+  };
+
+  index_buffer_.Create(props, ptr);
+}
